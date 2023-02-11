958eabc3a45411521be55bcd00a342b66ac6def6
KOSM1K <rob.357.ot@gmail.com>
2023-02-11 15:55:30 +0300

    Snake
    Snake base + it's test in main

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+from BackEnd.SnakeClass import Snake
+
 if __name__ == '__main__':
-    pass
+    snTest = Snake([(i, 0) for i in range(3, -1, -1)])
+    while True:
+        print(snTest.HPointer, snTest.TPointer, snTest.listOfCoords_H2T())
+        snTest.move(int(input()))
