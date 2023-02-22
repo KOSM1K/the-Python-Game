6d201e83c05b4ef68ccd667f58c557ce3485567e
KOSM1K <rob.357.ot@gmail.com>
2023-02-22 18:43:53 +0300

    unified
    updated for adding more snakes with unified control

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from Frontend import MainWindow
 
+<<<<<<< Updated upstream
 
 if __name__ == "__main__":
   MainWindow.main()
+=======
+if __name__ == '__main__':
+    pass
+>>>>>>> Stashed changes
