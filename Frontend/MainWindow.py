0416729c1be5edb82cb809797386c36ec98876f5
KOSM1K <rob.357.ot@gmail.com>
2023-02-22 19:59:07 +0300

    frontend+
    normal resize
    bank area on the right
    visual separation of snake field

--- Frontend/MainWindow.py
+++ Frontend/MainWindow.py
@@ -1,17 +1,28 @@
 import pygame
-import time
+from math import ceil
 from BackEnd.SnakeClass import Snake
 from BackEnd.FieldClass import Field
 
 FPS = 200
 VELOCITY = 1
 WALKEVERY = 10
+FIELD_SIZE = 40
+
 
 def main():
     pygame.init()
-    sc = pygame.display.set_mode((600, 600), pygame.RESIZABLE)
+    screen = pygame.display.set_mode((810, 610), pygame.RESIZABLE)
     pygame.display.set_caption('the Python Game (v1.0.0) (alpha 0004)')
     pygame.display.set_icon(pygame.image.load('Frontend/icon.png'))
+
+
+
+    field = Field([], [], (FIELD_SIZE, FIELD_SIZE))
+    sn = Snake([(i, 0) for i in range(15, -1, -1)], VELOCITY)
+    field.appendSnake(sn, 0)
+    NUM_OF_SNAKES = len(field.snakes)
+
+    playingField = resizePrepare(field, screen)
 
     running = True
     cnt = 1
@@ -24,14 +35,6 @@ def main():
           pygame.K_a: 2,
           pygame.K_UP: 3,
           pygame.K_w: 3}
-
-
-    field = Field([], [])
-
-    sn = Snake([(i, 0) for i in range(15, -1, -1)], VELOCITY)
-    field.appendSnake(sn, 0)
-
-    NUM_OF_SNAKES = len(field.snakes)
 
     clock = pygame.time.Clock()
     while running:
@@ -47,7 +50,7 @@ def main():
                 if event.key in ck.keys():
                     controls.remove(ck[event.key])
             elif event.type == pygame.WINDOWRESIZED:
-                pygame.display.update()
+                playingField = resizePrepare(field, screen)
 
         # moving snake if needed
         if cnt == 0:
@@ -61,17 +64,37 @@ def main():
                 if field.snakes[sn_ind].find_crossover() != -1:
                     running = False
 
-            width, height = sc.get_size()
-            sc.fill((0, 0, 0))
-            for sn_ind in range(NUM_OF_SNAKES):
-                cords2Draw = field.snakes[sn_ind].listOfCoords_H2T()
-                for i in cords2Draw:
-                    pygame.draw.rect(sc, 'green', (i[0] / 40 * width, i[1] / 40 * height, 1 / 40 * width, 1 / 40 * height))
-            pygame.draw.rect(sc, 'red',
-                             (field.apple[0] / 40 * width, field.apple[1] / 40 * height, 1 / 40 * width, 1 / 40 * height))
-
-            pygame.display.update()
+            draw_field(field, screen, playingField)
         clock.tick(FPS)
         cnt += 1
         cnt %= WALKEVERY
     pygame.quit()
+
+
+def resizePrepare(field: Field, screen):
+    width, height = screen.get_size()
+    size = min(width - 200, height)
+    playingField = pygame.Surface((size - 10, size - 10))
+    pygame.draw.rect(screen, 'cyan',
+                     (0, 0, size, size))
+    draw_field(field, screen, playingField)
+    return playingField
+
+
+def draw_field(field: Field, screen, playingField):
+    NUM_OF_SNAKES = len(field.snakes)
+    width, height = playingField.get_size()
+    fieldW, fieldH = field.size
+    playingField.fill((0, 0, 0))
+    for sn_ind in range(NUM_OF_SNAKES):
+        cords2Draw = field.snakes[sn_ind].listOfCoords_H2T()
+        for i in cords2Draw:
+            pygame.draw.rect(playingField, 'green',
+                             (i[0] / fieldW * width, i[1] / fieldH * height, ceil(1 / fieldW * width),
+                              ceil(1 / fieldH * height)))
+    pygame.draw.rect(playingField, 'red',
+                     (field.apple[0] / fieldW * width, field.apple[1] / fieldH * height, 1 / fieldW * width,
+                      1 / fieldH * height))
+
+    screen.blit(playingField, (5, 5))
+    pygame.display.update()
