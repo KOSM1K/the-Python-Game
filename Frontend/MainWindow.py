c45fc16c4ecbdfefa7b9408149a395779cfba0b0
KOSM1K <rob.357.ot@gmail.com>
2023-05-14 23:42:10 +0300

    back to local + in progress improvments

--- Frontend/MainWindow.py
+++ Frontend/MainWindow.py
@@ -2,28 +2,30 @@ import pygame
 from math import ceil
 from BackEnd.SnakeClass import Snake
 from BackEnd.FieldClass import Field
+from BackEnd.EventHandlerClass import EventHandler
 
-FPS = 200
-VELOCITY = 1
-WALKEVERY = 10
-FIELD_SIZE = 40
+SPS = 20  # steps per second
+FIELD_SIZE = 40  # length of side of square
+
+# DO NOT CHANGE!
+lenOfStep = 1  # length by one step
 
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((810, 610), pygame.RESIZABLE)
-    pygame.display.set_caption('the Python Game (v1.0.0) (alpha 0004)')
+    screen = pygame.display.set_mode((610, 610), pygame.RESIZABLE)
+    pygame.display.set_caption('the Python Game')
     pygame.display.set_icon(pygame.image.load('Frontend/icon.png'))
 
     field = Field([], [], (FIELD_SIZE, FIELD_SIZE))
-    sn = Snake([(i, 0) for i in range(15, -1, -1)], VELOCITY)
+    sn = Snake([(i, 0) for i in range(15, -1, -1)], lenOfStep)
     field.appendSnake(sn, 0)
     NUM_OF_SNAKES = len(field.snakes)
+    EvHndl = EventHandler()
 
     playingField = resizePrepare(field, screen)
 
     running = True
-    cnt = 1
     controls = []
     ck = {pygame.K_RIGHT: 0,
           pygame.K_d: 0,
@@ -36,44 +38,33 @@ def main():
 
     clock = pygame.time.Clock()
     while running:
-        # event capture
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            EvHndl.analyseEvent(event)
+            if EvHndl.events['quit']:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key in ck.keys():
-                    if ck[event.key] not in controls:
-                        controls.append(ck[event.key])
-            elif event.type == pygame.KEYUP:
-                if event.key in ck.keys():
-                    controls.remove(ck[event.key])
-            elif event.type == pygame.WINDOWRESIZED:
+            elif EvHndl.events['resize']:
                 playingField = resizePrepare(field, screen)
 
         # moving snake if needed
-        if cnt == 0:
-            for sn_ind in range(NUM_OF_SNAKES):
-                cur = True
-                for i in controls[::-1]:
-                    if field.move_snake(sn_ind, i):
-                        cur = False
-                        break
-                if cur: field.move_snake(sn_ind, field.dirs[sn_ind])
-                if field.snakes[sn_ind].find_crossover() != -1:
-                    running = False
+        for sn_ind in range(NUM_OF_SNAKES):
+            for i in EvHndl.events['controls'][::-1] + [field.dirs[sn_ind]]:
+                if not field.move_snake(sn_ind, i):
+                    pass
+                else:
+                    break
+            if field.snakes[sn_ind].find_crossover() != -1:
+                running = False
 
             draw_field(field, screen, playingField)
-        clock.tick(FPS)
-        cnt += 1
-        cnt %= WALKEVERY
+        clock.tick(SPS)
     pygame.quit()
 
 
 def resizePrepare(field: Field, screen):
     width, height = screen.get_size()
-    size = min(width - 200, height)
+    size = min(width, height)
     playingField = pygame.Surface((size - 10, size - 10))
-    pygame.draw.rect(screen, 'cyan',
+    pygame.draw.rect(screen, 'gray',
                      (0, 0, size, size))
     draw_field(field, screen, playingField)
     return playingField
